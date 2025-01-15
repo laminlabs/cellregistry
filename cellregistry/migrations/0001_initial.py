cba4e4c024c7359a4b7b46859204445bec200972
Lukas Heumos <lukas.heumos@posteo.net>
2025-01-15 21:24:24 +0100

    🎨 Lnschema_core removal
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- cellregistry/migrations/0001_initial.py
+++ cellregistry/migrations/0001_initial.py
@@ -84,7 +84,7 @@ class Migration(migrations.Migration):
                 (
                     "created_by",
                     models.ForeignKey(
-                        default=lamindb.users.current_user_id,
+                        default=lamindb.base.users.current_user_id,
                         on_delete=django.db.models.deletion.PROTECT,
                         related_name="+",
                         to="lamindb.user",
